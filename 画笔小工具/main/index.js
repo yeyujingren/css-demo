d6909aaea45c7d77c4ae4a7592d34c14608e9266
yifeng.tao <yifeng.tao@luckincoffee.com>
2021-05-08 16:42:29 +0800

    init painting

--- 画笔小工具/main/index.js
+++ 画笔小工具/main/index.js
@@ -1,3 +1,10 @@
 window.onload = function () {
-   
+   const painting = document.getElementById('painting');
+   // 如果不支持，则给出提示，并返回
+   if (!painting.getContext) {
+     window.alert('您的浏览器暂时不支持canvas呦，请升级浏览器版本，或者更换其他浏览器🐶');
+     return;
+   }
+   painting.width = document.documentElement.clientWidth;
+   painting.height = document.documentElement.clientHeight;
 }
